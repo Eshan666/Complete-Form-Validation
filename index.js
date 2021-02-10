9c0d5eece3a1b80d85f6c773f90e30fa237bd8e3
Eshan Ahmed <ehsan.ahmed@northsouth.edu>
2021-02-10 15:20:12 +0600

    completed with displaying error messages

--- index.js
+++ index.js
@@ -1,64 +1,108 @@
-console.log("Heloooooeew")
+
 
 let username = document.getElementById("name");
 let email = document.getElementById("email");
 let number = document.getElementById("number");
 
+let validName = false;
+let validEmail = false;
+let validNumber = false;
 
-username.addEventListener("input",() =>{
-    console.log("name is blurred");
+
+username.addEventListener("input", () => {
+
     let regex = /^[a-z][a-zA-Z0-9]{2,10}$/;
 
     let str = username.value;
 
-    if(regex.test(str)){
-        console.log("it's matched");
+    if (regex.test(str)) {
+
 
         username.classList.remove("textbox");
+        return validName = true;
     }
-    else{
-        console.log("it's not matched");
+    else {
+
         username.classList.add("textbox");
+        return validName = false;
     }
 
-})  
+})
 
 
-email.addEventListener("input",() =>{
-    console.log("email is blurred");
+email.addEventListener("input", () => {
+
     let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
 
     let str = email.value;
 
-    if(regex.test(str)){
-        console.log("it's matched");
+    if (regex.test(str)) {
+
 
         email.classList.remove("textbox");
+        return validEmail = true;
     }
-    else{
-        console.log("it's not matched");
+    else {
+
         email.classList.add("textbox");
+        return validEmail = false;
     }
 
-})  
+})
 
 
-number.addEventListener("input",() =>{
-    console.log("number is blurred");
+number.addEventListener("input", () => {
+
     let regex = /^([0-9]){10}$/;
 
     let str = number.value;
 
-    if(regex.test(str)){
-        console.log("it's matched");
+    if (regex.test(str)) {
+
 
         number.classList.remove("textbox");
+        return validNumber = true;
     }
-    else{
-        console.log("it's not matched");
+    else {
+
         number.classList.add("textbox");
+        return validNumber = false;
     }
 
-})  
+})
 
+let submit = document.getElementById("submit");
 
+submit.addEventListener("click", validateForm);
+
+function validateForm(e) {
+    e.preventDefault();
+
+    let message = document.getElementById("message");
+
+    if (validName && validEmail && validNumber) {
+        message.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success</strong> Your data has been submitted!
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>`
+        setTimeout(() => {
+            message.innerHTML = "";
+        }, 5000)
+
+    }
+    else {
+        message.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Error</strong> Your data has not been submitted!
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>`
+        setTimeout(() => {
+            message.innerHTML = "";
+        }, 5000)
+
+    }
+}
+/*
+setTimeout(() => {
+    message.innerHTML = "";
+})
+*/
